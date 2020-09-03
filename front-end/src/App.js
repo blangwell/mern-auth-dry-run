1e5dc7f90a022dfa70bcac536665d7bf25b7ec5d
blangwell <b.langwell@outlook.com>
2020-09-03 15:08:09 -0700

    [Components] Add About and Footer Components

---
+++ front-end/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Welcome from './Components/Welcome';
+import About from './Components/About';
+import Footer from './Components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <About />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return(
+    <footer className="footer bg-dark">
+      <div className="container text-center">
+        <span class="text-muted">
+          @2020 Built with MERN during COVID
+        </span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
